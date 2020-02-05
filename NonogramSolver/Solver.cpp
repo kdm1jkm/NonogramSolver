f1fe98ac8e0b00324dd9a8fad2c669e0d7bf166a
kdm1jkm <kdm1jkmm@gmail.com>
2020-02-05 15:14:39 +0900

    Add constructor and destructor of Solver class

--- NonogramSolver/Solver.cpp
+++ NonogramSolver/Solver.cpp
@@ -1,1 +1,9 @@
 #include "Solver.h"
+
+Solver::Solver(int width, int height, vector<vector<int>> verticalNum, vector<vector<int>> horizentalNum)
+	:mWidth(width), mHeight(height), mHorizentalNum(horizentalNum), mVerticalNum(verticalNum), map(new cell[width * height]) {}
+
+Solver::~Solver()
+{
+	delete[] map;
+}
--- NonogramSolver/Solver.h
+++ NonogramSolver/Solver.h
@@ -1,10 +1,24 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solver
 {
+private:
+	enum class cell {
+		TRUE,
+		FALSE,
+		NONE
+	};
+	const int mWidth, mHeight;
+	const vector<vector<int>> mHorizentalNum, mVerticalNum;
+	const cell* map;
+public:
+	Solver(int width, int height, vector<vector<int>> verticalNum, vector<vector<int>> horizentalNum);
+
+	~Solver();
 };
 
